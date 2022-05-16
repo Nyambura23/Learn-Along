abfb26bf5f1806d8cd9cabe565c56baade474a50
Nyambura🌻 <bianca@Biancas-MacBook-Air.local>
2022-05-16 17:29:21 +0300

    update comment form

--- app/views.py
+++ app/views.py
@@ -1,8 +1,12 @@
 from email import message
 from random import random
-from flask import render_template
+from flask import render_template, redirect, url_for,abort,request
 from app import app
 from .request import get_quotes,get_quote
+from .models import comments
+from .forms import CommentForm
+
+Comment=comment.Comment
 
 # Views
 @app.route('/')
@@ -24,7 +28,6 @@ def movie(quote_id):
     '''
     return render_template('quote.html',id = quote_id)
 
-
 def index():
 
     '''
@@ -44,3 +47,21 @@ def quote(id):
     title = f'{quote.title}'
 
     return render_template('quote.html',title = title,quote = quote)
+
+
+
+
+# @main.route('/comment/<int:pitch_id>', methods = ['POST','GET'])
+# @login_required
+# def comment(pitch_id):
+#     form = CommentForm()
+#     pitch = Blog.query.get(pitch_id)
+#     all_comments = Comment.query.filter_by(pitch_id = pitch_id).all()
+#     if form.validate_on_submit():
+#         comment = form.comment.data 
+#         pitch_id = pitch_id
+#         user_id = current_user._get_current_object().id
+#         new_comment = Comment(comment = comment,user_id = user_id,pitch_id = pitch_id)
+#         new_comment.save_c()
+#         return redirect(url_for('.comment', pitch_id = pitch_id))
+#     return render_template('comment.html', form =form, pitch = pitch,all_comments=all_comments)
