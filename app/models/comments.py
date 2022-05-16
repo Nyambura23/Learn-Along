f0d2a0bcc0b1d2ce61ef7d9a9c503662f1f061cf
Nyambura🌻 <bianca@Biancas-MacBook-Air.local>
2022-05-16 22:49:30 +0300

    Create a new row where first we check if any COMMENT exists for that particular blog and if True we call our macro and pass in the comments list.

--- app/models/comments.py
+++ app/models/comments.py
@@ -1,3 +1,60 @@
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(255),unique = True,nullable = False)
+    email  = db.Column(db.String(255),unique = True,nullable = False)
+    secure_password = db.Column(db.String(255),nullable = False)
+    pass_secure = db.Column(db.String(255))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    blogs = db.relationship('Blog', backref='user', lazy='dynamic')
+    comment = db.relationship('Comment', backref='user', lazy='dynamic')
+    upvote = db.relationship('Upvote',backref='user',lazy='dynamic')
+    downvote = db.relationship('Downvote',backref='user',lazy='dynamic')
+
+
+    @property
+    def set_password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @set_password.setter
+    def password(self, password):
+        self.secure_password = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.secure_password,password) 
+    
+    def save_u(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+    
+    def __repr__(self):
+        return f'User {self.username}'
+
+class Blog(db.Model):
+    __tablename__ = 'blog'
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(255),nullable = False)
+    post = db.Column(db.Text(), nullable = False)
+    comment = db.relationship('Comment',backref='blog',lazy='dynamic')
+    upvote = db.relationship('Upvote',backref='blog',lazy='dynamic')
+    downvote = db.relationship('Downvote',backref='blog',lazy='dynamic')
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    time = db.Column(db.DateTime, default = datetime.utcnow)
+    
+    def save_p(self):
+        db.session.add(self)
+        db.session.commit()
+
+        
+    def __repr__(self):
+        return f'Blog {self.post}'
+
 class Comment:
 
     all_comments = []
@@ -15,5 +72,25 @@ class Comment:
 
 
     @classmethod
-    def clear_comments(cls):
-        Comment.all_comments.clear()
+    def get_comments(cls,id):
+
+        response = []
+
+        for comment in cls.all_comments:
+            if comment.blog_id == id:
+                response.append(comment)
+
+        return response
+
+class Subscriber(db.Model):
+    __tablename__='subscribers'
+
+    id=db.Column(db.Integer,primary_key=True)
+    email = db.Column(db.String(255),unique=True,index=True)
+
+    def save_subscriber(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f'Subscriber {self.email}'
--- app/views.py
+++ app/views.py
@@ -1,67 +1,94 @@
-from email import message
-from random import random
 from flask import render_template, redirect, url_for,abort,request
-from app import app
-from .request import get_quotes,get_quote
-from .models import comments
-from .forms import CommentForm
+from . import main
+from flask_login import login_required,current_user
+from app.models import User,Blog,Comment,Upvote,Downvote
+from .forms import UpdateProfile,BlogForm,CommentForm
+from .. import db,photos
 
-Comment=comment.Comment
-
-# Views
-@app.route('/')
+@main.route('/')
 def index():
+    blogs = Blog.query.all()
+   
+    
+    return render_template('index.html',blogs = blogs)
 
-    '''
-    View root page function that returns the index page and its data
-    '''
-    # Getting random quotes
-    random_quotes = get_quotes('random') 
-    title = 'Home - Welcome to Learn Along'
-    return render_template('index.html', title = title,random = random_quotes)
+@main.route('/create_new', methods = ['POST','GET'])
+@login_required
+def new_blog():
+    form = BlogForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        post = form.post.data
+        user_id = current_user
+        new_blog_object = Blog(post=post,user_id=current_user._get_current_object().id,category=category,title=title)
+        new_blog_object.save_p()
+        return redirect(url_for('main.index'))
+        
+    return render_template('create_blog.html', form = form)
 
-@app.route('/quote/<quote_id>')
-def movie(quote_id):
-
-    '''
-    View quote page function that returns the quote details page and its data
-    '''
-    return render_template('quote.html',id = quote_id)
-
-def index():
-
-    '''
-    View root page function that returns the index page and its data
-    '''
-
-    title = 'Home - Welcome to The best learning website online'
-    return render_template('index.html', title = title)
-
-@app.route('/quote/<int:id>')
-def quote(id):
-
-    '''
-    View quote page function that returns the quote details page and its data
-    '''
-    quote = get_quote(id)
-    title = f'{quote.title}'
-
-    return render_template('quote.html',title = title,quote = quote)
+@main.route('/comment/<int:blog_id>', methods = ['POST','GET'])
+@login_required
+def comment(blog_id):
+    form = CommentForm()
+    blog = Blog.query.get(blog_id)
+    all_comments = Comment.query.filter_by(blog_id = blog_id).all()
+    if form.validate_on_submit():
+        comment = form.comment.data 
+        blog_id = blog_id
+        user_id = current_user._get_current_object().id
+        new_comment = Comment(comment = comment,user_id = user_id,blog_id = blog_id)
+        new_comment.save_c()
+        return redirect(url_for('.comment', blog_id = blog_id))
+    return render_template('comment.html', form =form, blog = blog,all_comments=all_comments)
 
 
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    user_id = current_user._get_current_object().id
+    posts = Blog.query.filter_by(user_id = user_id).all()
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user,posts=posts)
+
+@main.route('/user/<name>/updateprofile', methods = ['POST','GET'])
+@login_required
+def updateprofile(name):
+    form = UpdateProfile()
+    user = User.query.filter_by(username = name).first()
+    if user == None:
+        abort(404)
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+        user.save_u()
+        return redirect(url_for('.profile',name = name))
+    return render_template('profile/update.html',form =form)
 
 
-# @main.route('/comment/<int:pitch_id>', methods = ['POST','GET'])
-# @login_required
-# def comment(pitch_id):
-#     form = CommentForm()
-#     pitch = Blog.query.get(pitch_id)
-#     all_comments = Comment.query.filter_by(pitch_id = pitch_id).all()
-#     if form.validate_on_submit():
-#         comment = form.comment.data 
-#         pitch_id = pitch_id
-#         user_id = current_user._get_current_object().id
-#         new_comment = Comment(comment = comment,user_id = user_id,pitch_id = pitch_id)
-#         new_comment.save_c()
-#         return redirect(url_for('.comment', pitch_id = pitch_id))
-#     return render_template('comment.html', form =form, pitch = pitch,all_comments=all_comments)
+@main.route('/user/<name>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(name):
+    user = User.query.filter_by(username = name).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',name=name))
+
+@main.route('/like/<int:id>',methods = ['POST','GET'])
+@login_required
+def like(id):
+    blog = blog.query.get(id)
+    new_vote = Upvote(user = current_user, blog_id=id)
+    new_vote.save()
+    return redirect(url_for('main.index',blog_id=id))
+
+@main.route('/dislike/<int:id>',methods = ['POST','GET'])
+@login_required
+def dislike(id):
+    blog = Blog.query.get(id)
+    new_downvote = Downvote(user = current_user, blog_id=id)
+    new_downvote.save()
+    return redirect(url_for('main.index',id = id))
