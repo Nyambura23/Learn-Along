5fccf5845699b7197a073f23360f4457ade25126
Nyambura🌻 <bianca@Biancas-MacBook-Air.local>
2022-05-17 00:51:52 +0300

    create a bd and initialize it

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ class Config:
     QUOTE_API_BASE_URL ='http://quotes.stormconsultancy.co.uk/random.json'
     # QUOTE_API_KEY = os.environ.get('QUOTE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://bianca:password@localhost/blog'
 
 class ProdConfig(Config):
 
