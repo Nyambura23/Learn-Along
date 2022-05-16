22a04738dce9065ea8aaea0c63cde5393e51112f
Nyambura🌻 <bianca@Biancas-MacBook-Air.local>
2022-05-17 01:23:46 +0300

    create registration form for new users

---
+++ app/auth/forms.py
@@ -0,0 +1,26 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField,BooleanField
+from wtforms.validators import InputRequired,Email,EqualTo, Length
+from wtforms import ValidationError
+from ..models import User
+
+class LoginForm(FlaskForm):
+    username = StringField('Username',validators=[InputRequired()])
+    password = PasswordField('Password',validators=[InputRequired()])
+    remember = BooleanField('Remember Me!')
+    submit = SubmitField('Login')
+
+class RegForm(FlaskForm):
+    username = StringField('Enter Your Username', validators=[InputRequired(), Length(min=4, max=20)])
+    email = StringField('Email Address', validators=[InputRequired(),Email()])
+    password = PasswordField('Password',validators = [InputRequired(), EqualTo('password_confirm',message = 'Passwords must match')])
+    password_confirm = PasswordField('Confirm Passwords',validators = [InputRequired()])
+    submit = SubmitField('Sign Up')
+
+    def validate_email(self,data_field):
+        if User.query.filter_by(email = data_field.data).first():
+            raise ValidationError(message="The Email has already been taken!")
+    
+    def validate_username(self, data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError(message="The username has already been taken")
--- app/models.py
+++ app/models.py
@@ -1,92 +1,94 @@
 from . import db,login_manager
-from flask_login import UserMixin,current_user
-from sqlalchemy.sql import func
-from datetime import datetime
+from flask_login import current_user,UserMixin
 from werkzeug.security import generate_password_hash,check_password_hash
+from datetime import datetime
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
 
-class User(UserMixin, db.Model):
+class User (UserMixin,db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255),unique = True,nullable = False)
-    email  = db.Column(db.String(255),unique = True,nullable = False)
-    secure_password = db.Column(db.String(255),nullable = False)
-    pass_secure = db.Column(db.String(255))
-    bio = db.Column(db.String(255))
-    profile_pic_path = db.Column(db.String())
-    blogs = db.relationship('Blog', backref='user', lazy='dynamic')
+    email = db.Column(db.String(255), unique = True,nullable = False)
+    bio = db.Column(db.String(255),default ='My default Bio')
+    profile_pic_path = db.Column(db.String(150),default ='default.png')
+    hashed_password = db.Column(db.String(255),nullable = False)
+    blog = db.relationship('Blog', backref='user', lazy='dynamic')
     comment = db.relationship('Comment', backref='user', lazy='dynamic')
-    upvote = db.relationship('Upvote',backref='user',lazy='dynamic')
-    downvote = db.relationship('Downvote',backref='user',lazy='dynamic')
-
-
+    
     @property
     def set_password(self):
         raise AttributeError('You cannot read the password attribute')
 
     @set_password.setter
     def password(self, password):
-        self.secure_password = generate_password_hash(password)
+        self.hashed_password = generate_password_hash(password)
 
-    def verify_password(self, password):
-        return check_password_hash(self.secure_password,password) 
-    
-    def save_u(self):
+
+    def verify_password(self,password):
+        return check_password_hash(self.hashed_password,password)
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return "User: %s" %str(self.username)
+
+class Blog(db.Model):
+    __tablename__ = 'blogs'
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(255),nullable=False)
+    content = db.Column(db.Text(),nullable=False)
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    comment = db.relationship('Comment', backref='blog', lazy='dynamic')
+
+    def save(self):
         db.session.add(self)
         db.session.commit()
 
     def delete(self):
         db.session.delete(self)
         db.session.commit()
-    
-    def __repr__(self):
-        return f'User {self.username}'
 
-class Blog(db.Model):
-    __tablename__ = 'blog'
-    id = db.Column(db.Integer, primary_key = True)
-    title = db.Column(db.String(255),nullable = False)
-    post = db.Column(db.Text(), nullable = False)
-    comment = db.relationship('Comment',backref='blog',lazy='dynamic')
-    upvote = db.relationship('Upvote',backref='blog',lazy='dynamic')
-    downvote = db.relationship('Downvote',backref='blog',lazy='dynamic')
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    time = db.Column(db.DateTime, default = datetime.utcnow)
-    
-    def save_p(self):
+    def get_blog(id):
+        blog = Blog.query.filter_by(id=id).first()
+
+        return blog
+
+    def __repr__(self):
+        return f'Blog {self.title}'
+
+class Comment(db.Model):
+    __tablename__='comments'
+
+    id = db.Column(db.Integer,primary_key = True)
+    comment = db.Column(db.String)
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
+    blog_id = db.Column(db.Integer,db.ForeignKey("blogs.id"))
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+
+    def save(self):
         db.session.add(self)
         db.session.commit()
 
-        
+    def delete(self):
+        db.session.remove(self)
+        db.session.commit()
+
+    def get_comment(id):
+        comment = Comment.query.all(id=id)
+        return comment
+
+
     def __repr__(self):
-        return f'Blog {self.post}'
-
-class Comment:
-
-    all_comments = []
-
-    def __init__(self,author,id,quote,permalink,comment):
-        self.author = author
-        self.id = id
-        self.quote = quote
-        self.permalink = permalink
-        self.comment = comment
-
-
-    def save_comment(self):
-        Comment.all_comments.append(self)
-
-
-    @classmethod
-    def get_comments(cls,id):
-
-        response = []
-
-        for comment in cls.all_comments:
-            if comment.blog_id == id:
-                response.append(comment)
-
-        return response
+        return f'Comment {self.comment}'
 
 class Subscriber(db.Model):
     __tablename__='subscribers'
@@ -100,16 +102,3 @@ class Subscriber(db.Model):
 
     def __repr__(self):
         return f'Subscriber {self.email}'
-
-
-class Quote:
-    '''
-    Quote class to define Quote Objects
-    '''
-
-    def __init__(self,author,id,quote,permalink):
-        self.author = author
-        self.id = id
-        self.quote = quote
-        self.permalink = 'http://quotes.stormconsultancy.co.uk/quotes/1'
-        
