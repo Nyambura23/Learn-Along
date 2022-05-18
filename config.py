59b77116019a024628f2473714d6fc200ee389e6
Nyambura🌻 <bianca@Biancas-MacBook-Air.local>
2022-05-18 17:26:50 +0300

    update quote request api

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 import os
 
 class Config:
-
     QUOTE_API_BASE_URL ='http://quotes.stormconsultancy.co.uk/random.json'
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://bianca:password@localhost/blog'
